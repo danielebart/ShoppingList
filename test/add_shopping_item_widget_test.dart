d9c4307fef2c0fa571bd0ecad7057b0183a2831c
Daniele Bartorilla <danielebartorilla@gmail.com>
2019-09-15 22:49:32 +0200

    add AddItemProvider tests

---
+++ test/add_shopping_item_widget_test.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:injector/injector.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shopping_list/main.dart';
+import 'package:shopping_list/shopping_list/add_item/add_item_provider.dart';
+import 'package:shopping_list/shopping_list/add_item/add_shopping_item_widget.dart';
+import 'package:shopping_list/shopping_list/items/shopping_list_provider.dart';
+import 'package:shopping_list/shopping_list/shopping_list_item.dart';
+
+void main() {
+  testWidgets('when pressing the add item button then show the add item widget',
+          (WidgetTester tester) async {
+        Injector injector = Injector.appInstance;
+        var addItemProviderMock = _MockAddItemProvider();
+        injector.registerDependency<ShoppingListProvider>((injector) {
+          return _MockShoppingListProvider();
+        });
+        injector.registerDependency<AddItemProvider>((injector) {
+          return addItemProviderMock;
+        });
+        await tester.pumpWidget(MyApp());
+        await tester.tap(find.byIcon(Icons.add_shopping_cart));
+        await tester.pumpAndSettle();
+
+        expect(find.byType(AddItemWidget), findsOneWidget);
+        injector.clearAll();
+      });
+
+  testWidgets(
+      'given AddItemDisabled state when opening the add sheet then '
+          'the add button is disabled', (WidgetTester tester) async {
+    Injector injector = Injector.appInstance;
+    var addItemProviderMock = _MockAddItemProvider();
+    injector.registerDependency<ShoppingListProvider>((injector) {
+      return _MockShoppingListProvider();
+    });
+    injector.registerDependency<AddItemProvider>((injector) {
+      return addItemProviderMock;
+    });
+    when(addItemProviderMock.state).thenReturn(AddItemDisabled());
+
+    await tester.pumpWidget(MyApp());
+    await tester.tap(find.byIcon(Icons.add_shopping_cart));
+    await tester.pumpAndSettle();
+
+    expect(
+        find.byWidgetPredicate(
+                (Widget widget) =>
+            widget is Icon && widget.color == Colors.grey),
+        findsOneWidget);
+    injector.clearAll();
+  });
+
+  testWidgets(
+      'given AddItemDisabled state when opening the add sheet then the add '
+          'button is disabled', (WidgetTester tester) async {
+    Injector injector = Injector.appInstance;
+    var addItemProviderMock = _MockAddItemProvider();
+    injector.registerDependency<ShoppingListProvider>((injector) {
+      return _MockShoppingListProvider();
+    });
+    injector.registerDependency<AddItemProvider>((injector) {
+      return addItemProviderMock;
+    });
+    when(addItemProviderMock.state).thenReturn(AddItemDisabled());
+
+    await tester.pumpWidget(MyApp());
+    await tester.tap(find.byIcon(Icons.add_shopping_cart));
+    await tester.pumpAndSettle();
+
+    expect(
+        find.byWidgetPredicate(
+                (Widget widget) =>
+            widget is Icon && widget.color == Colors.grey),
+        findsOneWidget);
+    injector.clearAll();
+  });
+
+  testWidgets(
+      'given AddItemEnabled state when clicking on add button'
+          ' then verify button press call', (WidgetTester tester) async {
+    Injector injector = Injector.appInstance;
+    var addItemProviderMock = _MockAddItemProvider();
+    injector.registerDependency<ShoppingListProvider>((injector) {
+      return _MockShoppingListProvider();
+    });
+    injector.registerDependency<AddItemProvider>((injector) {
+      return addItemProviderMock;
+    });
+    when(addItemProviderMock.state).thenReturn(AddItemEnabled());
+
+    await tester.pumpWidget(MyApp());
+    await tester.tap(find.byIcon(Icons.add_shopping_cart));
+    await tester.pumpAndSettle();
+    await tester.enterText(find.byType(TextField), "whatever");
+    await tester.tap(find.byType(IconButton));
+    await tester.pump();
+
+    verify(addItemProviderMock.addButtonPressed());
+    injector.clearAll();
+  });
+
+  testWidgets(
+      'given AddItemEnabled state when typing text'
+          ' then verify typing text changed call', (WidgetTester tester) async {
+    Injector injector = Injector.appInstance;
+    var addItemProviderMock = _MockAddItemProvider();
+    injector.registerDependency<ShoppingListProvider>((injector) {
+      return _MockShoppingListProvider();
+    });
+    injector.registerDependency<AddItemProvider>((injector) {
+      return addItemProviderMock;
+    });
+    when(addItemProviderMock.state).thenReturn(AddItemEnabled());
+
+    await tester.pumpWidget(MyApp());
+    await tester.tap(find.byIcon(Icons.add_shopping_cart));
+    await tester.pumpAndSettle();
+    await tester.enterText(find.byType(TextField), "whatever");
+    await tester.pump();
+
+    verify(addItemProviderMock.onTextChanged("whatever"));
+    injector.clearAll();
+  });
+
+  testWidgets(
+      'given AddItemSuccess state when opening the add sheet then return the '
+          'added item to the ShoppingListProvider', (
+      WidgetTester tester) async {
+    Injector injector = Injector.appInstance;
+    var addItemProviderMock = _MockAddItemProvider();
+    var shoppingListProviderMock = _MockShoppingListProvider();
+    injector.registerDependency<ShoppingListProvider>((injector) {
+      return shoppingListProviderMock;
+    });
+    injector.registerDependency<AddItemProvider>((injector) {
+      return addItemProviderMock;
+    });
+    var addedItem = ShoppingListItem(
+        id: "fake-id",
+        listId: "fake-list-id",
+        flagged: true,
+        title: "fake-title");
+    when(addItemProviderMock.state).thenReturn(AddItemSuccess(addedItem));
+
+    await tester.pumpWidget(MyApp());
+    await tester.tap(find.byIcon(Icons.add_shopping_cart));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(AddItemWidget), findsNothing);
+    verify(shoppingListProviderMock.onItemAdded(addedItem));
+    verify(addItemProviderMock.setCloseState());
+    injector.clearAll();
+  });
+}
+
+class _MockShoppingListProvider extends Mock
+    with ChangeNotifier
+    implements ShoppingListProvider {}
+
+class _MockAddItemProvider extends Mock
+    with ChangeNotifier
+    implements AddItemProvider {}
---
+++ test/shopping_list_widget_test.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shopping_list/shopping_list/items/shopping_item_widget.dart';
+import 'package:shopping_list/shopping_list/items/shopping_list_provider.dart';
+import 'package:shopping_list/shopping_list/items/shopping_list_widget.dart';
+import 'package:shopping_list/shopping_list/shopping_list.dart';
+import 'package:shopping_list/shopping_list/shopping_list_item.dart';
+
+import 'widget_utils.dart';
+
+void main() {
+  testWidgets(
+      'given populated list of item when adding multiple ites then they are displayed in the list',
+      (WidgetTester tester) async {
+    var fakeItem1 = ShoppingListItem(
+        id: "zenzero", listId: "", flagged: true, title: "zenzero");
+    var fakeItem2 = ShoppingListItem(
+        id: "pizza", listId: "", flagged: false, title: "pizza");
+    var streamController = StreamController<ShoppingList>(sync: false);
+    var fakeShoppingList = ShoppingList("", [
+      ShoppingListItem(
+          id: "zenzero", listId: "", flagged: true, title: "zenzero"),
+      ShoppingListItem(id: "pizza", listId: "", flagged: false, title: "pizza")
+    ]);
+    var shoppingListProviderMock = _MockShoppingListProvider();
+    when(shoppingListProviderMock.findByID("zenzero")).thenReturn(fakeItem1);
+    when(shoppingListProviderMock.findByID("pizza")).thenReturn(fakeItem2);
+    when(shoppingListProviderMock.list)
+        .thenAnswer((_) => streamController.stream);
+
+    await tester.pumpWidget(MaterialTestProvider<ShoppingListProvider>(
+      child: ShoppingListWidget(),
+      provider: shoppingListProviderMock,
+    ));
+    streamController.add(fakeShoppingList);
+    await tester.pump(Duration.zero);
+
+    expect(
+        find.byWidgetPredicate((Widget widget) =>
+            widget is CheckboxListTile &&
+            (widget.title as Text).data == "zenzero" &&
+            widget.value),
+        findsOneWidget);
+    expect(
+        find.byWidgetPredicate((Widget widget) =>
+            widget is CheckboxListTile &&
+            (widget.title as Text).data == "pizza" &&
+            !widget.value),
+        findsOneWidget);
+    await streamController.close();
+  });
+
+  testWidgets(
+      'given a populated list of item when removing an item then remove from it from the list',
+      (WidgetTester tester) async {
+    var fakeItemId = "fake-id";
+    var fakeItem = ShoppingListItem(
+        id: fakeItemId, listId: "", flagged: true, title: "zenzero");
+    var streamController = StreamController<ShoppingList>(sync: false);
+    var fakeShoppingList = ShoppingList("", [
+      ShoppingListItem(
+          id: fakeItemId, listId: "", flagged: true, title: "zenzero")
+    ]);
+    var shoppingListProviderMock = _MockShoppingListProvider();
+    when(shoppingListProviderMock.findByID(fakeItemId)).thenReturn(fakeItem);
+    when(shoppingListProviderMock.list)
+        .thenAnswer((_) => streamController.stream);
+
+    await tester.pumpWidget(MaterialTestProvider<ShoppingListProvider>(
+      child: ShoppingListWidget(),
+      provider: shoppingListProviderMock,
+    ));
+
+    streamController.add(fakeShoppingList);
+    await tester.pump(Duration.zero);
+    await tester.drag(find.text("zenzero"), Offset(500.0, 0.0));
+    await tester.pumpAndSettle();
+
+    verify(shoppingListProviderMock.removeItem(fakeItemId));
+    await streamController.close();
+  });
+
+  testWidgets(
+      'given a populated list of item when flagging an item then call provider flag method',
+      (WidgetTester tester) async {
+    var fakeItemId = "fake-id";
+    var fakeItem = ShoppingListItem(
+        id: fakeItemId, listId: "", flagged: true, title: "zenzero");
+    var streamController = StreamController<ShoppingList>(sync: false);
+    var fakeShoppingList = ShoppingList("", [
+      ShoppingListItem(
+          id: fakeItemId, listId: "", flagged: true, title: "zenzero")
+    ]);
+    var shoppingListProviderMock = _MockShoppingListProvider();
+    when(shoppingListProviderMock.findByID(fakeItemId)).thenReturn(fakeItem);
+    when(shoppingListProviderMock.list)
+        .thenAnswer((_) => streamController.stream);
+
+    await tester.pumpWidget(MaterialTestProvider<ShoppingListProvider>(
+      child: ShoppingListWidget(),
+      provider: shoppingListProviderMock,
+    ));
+    streamController.add(fakeShoppingList);
+    await tester.pump(Duration.zero);
+    await tester.tap(find.byType(ShoppingItemWidget));
+    await tester.pump();
+
+    verify(shoppingListProviderMock.setFlagged(fakeItemId, false));
+    await streamController.close();
+  });
+}
+
+class _MockShoppingListProvider extends Mock
+    with ChangeNotifier
+    implements ShoppingListProvider {}
